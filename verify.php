2cc495e1b09f1b2a31402f7fd7c71aa5d499e830
anuchasmct <38835048+anuchasmct@users.noreply.github.com>
2018-04-29 22:21:25 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'uFGEJYG4XiBO5kqPzxO/1srV2YjjQMrjSNj1vN6xYgKSoEVz0hwJCnqRQszWcHvv+tustrIeyLm25XwtO81og80hqDCHb5pojQ9H0nEIOkp3yKyJzUgarCsszfxoh9Xy8VeOMnVd4wvDNNP8U7xvpQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
